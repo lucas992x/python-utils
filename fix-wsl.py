c2c9dafa5a20d4fd1753ef64cda889a57338b64e
lucas992x <lucas992x@gmx.com>
2022-05-01 23:43:35 +0200

    Improving fix-wsl.py

--- fix-wsl.py
+++ fix-wsl.py
@@ -1,18 +1,30 @@
+import sys, os.path
 '''
-This script edits two files in Pywikibot directory to make it work with WSL. No
-argument is required, it just needs to be launched in Pywikibot directory.
-Source (with little changes because Pywikibot has changed since then):
-https://stackoverflow.com/questions/32760041/pywikibot-how-to-handle-user-config-py-owned-by-someone-else/62986377#62986377
+This script edits two files in Pywikibot directory to make it work with WSL; it
+is idempotent, i.e. executing it multiple times is the same as executing it
+once. It can be launched without arguments when executed in pywikibot directory,
+or passing it pywikibot directory's path. Examples:
+
+python fix-wsl.py /mnt/d/pywikibot
+python /mnt/d/python/fix-wsl.py
+
+Credit: https://stackoverflow.com/questions/32760041/pywikibot-how-to-handle-user-config-py-owned-by-someone-else/62986377#62986377
 '''
-file1 = 'pywikibot/config.py'
+# get paths of files
+if len(sys.argv) > 1:
+    file1 = os.path.join(sys.argv[1], 'pywikibot', 'config.py')
+    file2 = os.path.join(sys.argv[1], 'pywikibot', 'tools', '__init__.py')
+else:
+    file1 = os.path.join('pywikibot', 'config.py')
+    file2 = os.path.join('pywikibot', 'tools', '__init__.py')
+# edit file1
 with open(file1, 'r') as file:
     text = file.read()
 text = text.replace('if not OSWIN32 and _fileuid not in [os.getuid(), 0]:', 'if not OSWIN32 and _fileuid not in [os.getuid(), 0] and False:')
 text = text.replace('elif OSWIN32 or _filemode & 0o02 == 0:', 'elif OSWIN32 or _filemode & 0o02 == 0 or True:')
 with open(file1, 'w') as file:
     file.write(text)
-
-file2 = 'pywikibot/tools/__init__.py'
+# edit file2
 with open(file2, 'r') as file:
     text = file.read()
 if not '#os.chmod(filename, mode)' in text:
