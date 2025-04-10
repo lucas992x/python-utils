aa9f4ef1ae257dec6fd4970a27ec49a17da5d8f7
lucas992x <lucas992x@gmx.com>
2025-04-10 17:28:34 +0200

    Expanded count-days.py

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 This repository contains some utilities written in Python, specifically:
 - `batch-download.py` downloads a list of files reading them from a text file. It works with URLs, but can also be used with filenames from any MediaWiki-based website: in this case list can be retrieved using Pywikibot's [`listpages`](https://www.mediawiki.org/wiki/Manual:Pywikibot/listpages.py) with `-format:3` to format it properly and `-cat:"<category name>"`, `-imagesused:"<page title>"` or any other generator/filter.
+- `count-days.py` can add/subtract days to a date or compute the difference in days between two dates.
 - `fix-wsl.py` edits two files in Pywikibot repository to make it work with Windows Subsystem for Linux.
 - `rename-pairs.py` renames all files in given directory reading replacements from a text file.
--- count-days.py
+++ count-days.py
@@ -1,32 +1,56 @@
 import argparse
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 """
-A simple script that computes the difference in days between two dates.
+A simple script that can add/subtract days to a date or compute a difference in days between two dates.
 
-Arguments:
+Arguments used to add/subtract days to a date:
+--start: start date, defaults to current date if not provided.
+--diff: number of days to add (if negative will be subtracted).
+
+Arguments to compute difference in days between two dates:
 --start: start date, defaults to current date if not provided.
 --end: end date, defaults to current date if not provided.
---format: format of both start and end date, by default %Y%m%d is used, see
+
+Common arguments:
+--format: format of dates, by default %Y%m%d is used, see
 https://docs.python.org/3/library/datetime.html#format-codes for further info.
 --message: message that will be printed.
---placeholder: piece of text in output message that will be replaced by days difference.
+--placeholder: piece of text in output message that will be replaced by desired information.
 """
+
+
+# parse a string to obtain a date, returning current date if input string is null or empty
+def parse_date_string(date_string, date_format):
+    if date_string:
+        parsed_date = datetime.strptime(date_string, date_format).date()
+    else:
+        parsed_date = date.today()
+    return parsed_date
 
 
 # given two dates, compute difference in days (will be negative if end is before start)
-def compute_days_difference(start_date_string, end_date_string, dates_format):
-    # set start/end date as current date if not provided
-    if start_date_string:
-        start_date = datetime.strptime(start_date_string, dates_format).date()
-    else:
-        start_date = date.today()
-    if end_date_string:
-        end_date = datetime.strptime(end_date_string, dates_format).date()
-    else:
-        end_date = date.today()
+def compute_days_difference(start_date_string, end_date_string, date_format):
+    # parse start and end date
+    start_date = parse_date_string(start_date_string, date_format)
+    end_date = parse_date_string(end_date_string, date_format)
     # return difference in days
     return (end_date - start_date).days
+
+
+# given a date, add/subtract specified number of days
+def add_days(start_date_string, date_format, days):
+    start_date = parse_date_string(start_date_string, date_format)
+    new_date = start_date + timedelta(days=days)
+    new_date_string = new_date.strftime(date_format)
+    return new_date_string
+
+
+# print a message replacing placeholder string with desired infomation
+def print_message(message, default_message, placeholder, replacement):
+    if not message:
+        message = default_message
+    print(message.replace(placeholder, replacement))
 
 
 # main function
@@ -34,14 +58,25 @@ def main():
     # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--start", default="")
+    parser.add_argument("--diff", default="")
     parser.add_argument("--end", default="")
-    parser.add_argument("--format", default="%Y%m%d")
-    parser.add_argument("--message", default="Difference in days is <>")
+    parser.add_argument("--message", default="")
     parser.add_argument("--placeholder", default="<>")
+    parser.add_argument("--format", default="%Y%m%d")
     args = parser.parse_args()
-    # compute difference
-    days_difference = compute_days_difference(args.start, args.end, args.format)
-    print(args.message.replace(args.placeholder, str(days_difference)))
+    # add/subtract days if requested
+    if args.diff:
+        new_date = add_days(args.start, args.format, int(args.diff))
+        print_message(args.message, "Computed date is <>", args.placeholder, new_date)
+    # compute difference if requested
+    else:
+        days_difference = compute_days_difference(args.start, args.end, args.format)
+        print_message(
+            args.message,
+            "Difference in days is <>",
+            args.placeholder,
+            str(days_difference),
+        )
 
 
 # invoke main function
